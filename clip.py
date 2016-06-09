34e41ff4b98896c104d04e5797d5c8c733585902
Nastia Martyniuk <anastasia.20.12.95@rambler.ru>
2016-06-09 12:37:32 +0300

    Сliping the project workspace (Vyshgorod and Bila Tserkva).

---
+++ clip.py
@@ -0,0 +1,7 @@
+import arcpy
+arcpy.env.workspace = r"E:\DZZ\practice"
+
+arcpy.CheckOutExtension("Data Management")
+arcpy.Clip_management("n50_e030_1arc_v3.tif","30.401345 50.571615 30.527159 50.648948", "clip_vysh2.tif","frame_vyshgorod.shp", "#", "ClippingGeometry","NO_MAINTAIN_EXTENT")
+arcpy.Clip_management("n49_e030_1arc_v3.tif","30,031388 49,770681 30,120094 49,829311", "clip_bila2.tif","frame_bila.shp", "#", "ClippingGeometry","NO_MAINTAIN_EXTENT")
+
