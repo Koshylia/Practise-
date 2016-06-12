f45af069a4e1e6ff4e0c1227155a83727d9fbd9b
Ksenia <koshylia@gmail.com>
2016-06-12 21:12:36 +0300

    Added comment and modifications to the function that filters the raster using std/ It also removes depressions

--- Filtrations.py
+++ Filtrations.py
@@ -6,40 +6,48 @@ Rasterband= r"C:\Users\K\Desktop\Practise\vush30m"
 RasterName = "vush30m"
 
 
+
 def workSpace(overwrite, workspace):
+    # This function works with enviromets settings in the Arcgis
     arcpy.CheckOutExtension("Spatial")
     arcpy.env.overwriteOutput = overwrite
     arcpy.env.workspace = workspace
 
 
 def rasterHeight(Rasterband):
+    #This function determines raster height
     raster = arcpy.Raster(Rasterband)
     endi = raster.height
     return endi
 
 
 def rasterWidth(Rasterband):
+    #This function determines raster width
     raster = arcpy.Raster(Rasterband)
     endj = raster.width
     return endj
 
 
 def cellSize(Rasterband):
+    #This function determines raster cell size
     raster = arcpy.Raster(Rasterband)
     cellsize = raster.meanCellHeight
     return cellsize
 
 def leftCorner(Rasterband):
+    #this function determines position of the raster left corner
     coner = arcpy.Point(raster.extent.XMin, raster.extent.YMin)
     return coner
 
 
 def rasterArray(Rasterband):
+    #this function creates array with include information about each pixel
     raster = arcpy.Raster(Rasterband)
     arr = arcpy.RasterToNumPyArray(raster, nodata_to_value=None)
     return arr
 
 def filtration(r,Rasterband):
+    #This feature filters the raster method and removes depression
     newarr=[]
     for i in range(0,rasterHeight(Rasterband)):
         G=[]
@@ -49,16 +57,16 @@ def filtration(r,Rasterband):
             meanArr=numpy.mean(a)
             ListArr=a.tolist()
             List=[]
-            for k in range(len(ListArr)):
-                for t in range(len(ListArr[0])):
-                    if ListArr[k][t]>=(meanArr-stdArr) and ListArr[k][t]<=(meanArr+stdArr):
-                        List.append(ListArr[k][t])
-            if len(List)>0:
-                if List[len(List)/2]==min(List)and len(List)>1:
-                    g=float(sum(List)-List[len(List)/2])/(len(List)-1)
-                else:
-                    g=float(sum(List)/len(List))
-                G.append(g)
+            f for k in range(len(ListArr)):
+            for t in range(len(ListArr[0])):
+                if ListArr[k][t]>=(meanArr-stdArr) and ListArr[k][t]<=(meanArr+stdArr):
+                    List.append(ListArr[k][t])
+        if len(List)>0:
+            if List[int(len(List)/2.0)]is min(List)and len(List)>1:
+                g=float(sum(List)-List[int(len(List)/2.0)])/(len(List)-1)
+            else:
+                g=float(sum(List)/len(List))
+            G.append(g)
         newarr.append(G)
     array=numpy.array(newarr)
     myRaster = arcpy.NumPyArrayToRaster(array,lower_left_corner=coner,x_cell_size=selsize)
