f0a963f87a2dcff2280f11d96801ce3d26404fd0
Ksenia <koshylia@gmail.com>
2016-06-12 20:59:48 +0300

    Added coments and added some modification to the determining excess

--- single flow direction.py
+++ single flow direction.py
@@ -1,20 +1,27 @@
 import arcpy
 from arcpy.sa import *
 import numpy
+import math
+#environment settings in the ArcGis
 arcpy.CheckOutExtension("Spatial")
 arcpy.env.overwriteOutput = True
 Rasterband= r"C:\Users\K\Desktop\Practise\FilterVush30m.tif"
 arcpy.env.workspace=r"C:\Users\K\Desktop\Practise"
 arcpy.CheckOutExtension("Spatial")
+# Raster properties
 raster=arcpy.Raster(Rasterband)
 endi=raster.height
 endj=raster.width
 selsize=raster.meanCellHeight
 coner=arcpy.Point(raster.extent.XMin,raster.extent.YMin)
 Spatref=raster.spatialReference
+#radius of the movving window
 r=1
+#convert raster to the array for working with each pixel
 arr=arcpy.RasterToNumPyArray(raster,nodata_to_value=None)
+#creation new array for aplying values of each pixel to creation new raster that will include information about the direction
 newarr=[]
+# c is a matrix of direction 32=NW 64=N 128=NE 1=E 2=SE 4=S 8=SW 16=W
 c=[32,64,128,16,"",1,8,4,2]
 for i in range(0,endi):
     G=[]
@@ -25,11 +32,16 @@ for i in range(0,endi):
         for k in range(len(ListArr)):
             for t in range(len(ListArr[0])):
                 if len(ListArr[0])>1:
+                    #count excess for each pixel
                     Per= float(ListArr[int(len(ListArr)/2.0)][int(len(ListArr[0])/2.0)]-ListArr[k][t])
                     List.append(Per)
                 else:
                     List.append(ListArr[k][t])
+        #determine max excess
         a=max(List)
+        if list.count(max(List))!=1:
+            List=List*math.sqrt(2)
+            a=max(List)
         ind=List.index(a)
         g=c[ind]
         G.append(g)
